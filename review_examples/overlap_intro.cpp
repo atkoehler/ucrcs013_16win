a4bcb842ecb4d8231f8a3a1daefda02e86ed46db
Adam <akoeh001@cs.ucr.edu>
2016-01-07 01:56:39 +0000

    updated file

--- review_examples/overlap_intro.cpp
+++ review_examples/overlap_intro.cpp
@@ -3,15 +3,40 @@
 // In class assignment & exercise - Overlap
 
 // what is the point of the assignment (what does it do)
+Find the overlap of the words, match up sequences of 
+chromosomes.
 
 // what has been simplified?
+change chromosomes to words/strings
 
-// which page is the algorithm on ?
+// which page is the algorithm on ? 
+page 2 of 3
 // is the algorithm in English, pseudocode, or C++?
+English
 
-// what string functions do we use, where in the specification is this stated?
+
+// what string functions do we use, where in the 
+// specification is this stated?
+page 3 of 3
+find()
+size()/length()
+substr()
+
 
 // what C++ syntax pieces will we need? 
 // will we require loops?
+loops 
+for - to go over individual words
+while - page 3 to go over all the words in input
+functions
+vector(s)
+logic (bool, if, else, etc)
+& (referenc parameters may be useful)
 
-// do we have any knowledge gaps that prohibit implementation?
+// do we have any knowledge gaps that prohibit 
+// implementation?
+how do we get the file of words in?
+while cin what???
+
+
+
