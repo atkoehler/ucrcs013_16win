1c9d146213be59fecacd299453ca0ec81efb20d5
Adam <akoeh001@cs.ucr.edu>
2017-03-03 23:51:15 +0000

    added comments

--- classes/moneyExample/main.cpp
+++ classes/moneyExample/main.cpp
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string>
 #include "money.h"
+
 
 using namespace std;
 
@@ -15,9 +17,29 @@ int main() {
     Money money2 = Money(1, 99);
     cout << money.convertToCents() << endl;
     cout << money.convertToDollars() << endl;
+    
+    // const return type prevents assignment into
+    //      a returned object such as:
     // money.add(money2) = money;
+    
+    // We cannot do the following either:
+    //  7 is a literal so it cannot be changed as it is
+    //      not a spot in memory
     // 7 = x + y;
+    
+    // x+y yields a return value and return values are 
+    //  stored in memory, but again const will prevent
+    //  alteration and this isn't an object so we could
+    //  not change it under normal circumstances as it
+    //  is not a named space in memory (variable or object)
     // x + y = 5;
+    
+    // at() - does not have const in return type
+    //      for at least one function implementation
+    //      to allow it to exist on the left hand side
+    //      of an assignment statement
+    // v.at(0) = 5;
+    // x = v.at(0);
     
     
     cout << money.add(money2).convertToDollars() << endl;
--- classes/moneyExample/money.h
+++ classes/moneyExample/money.h
@@ -9,15 +9,50 @@ class Money {
     private:
         int dollars;
         int cents;
+        
+        // this is a private function, this means that
+        //  a user of the object cannot invoke it directly
+        //  such as Money m1; m1.init();
+        // Private functions are usually helper functions
+        //  for publicly available calls, in this case
+        //  init() makes sure that no Money object is 
+        //  initialized with over 99 cents because that
+        //  should tabulate into the dollars category.
         void init();
+        
     public:
         Money();
+        
+        // without variable names which order the arguments
+        //  exist is hard to determine; so is it dollars then cents,
+        //  or cents then dollars?
+        // variable names are not required but they help avoid
+        //  confusing scenarios such as this one
         Money(int, int);
+        
         Money(int);
         int convertToCents() const;
         double convertToDollars() const;
+        
+        // 3 Forms of const
+        // const before the return type prevents the
+        //      function call from existing on the left
+        //      hand side of an assignment statement
+        //      as the returned object cannot be changed
+        // const as part of a parameter prevents the 
+        //      parameter's value from changing
+        // const after the parameters list prevents
+        //      the function from changing the values of
+        //      the object's private data members
         const Money add(const Money &) const;
+        
+        // operator overloading allows us to use the 
+        //  well known operators instead of calling
+        //  the function using the dot member function 
+        //  invocation style
+        //  so: m1.add(m2) becomes m1 + m2
         const Money operator+(const Money &) const;
+        
         const Money subtract(const Money &) const;
 };
 #endif
